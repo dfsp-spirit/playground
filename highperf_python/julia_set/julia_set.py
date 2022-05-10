d55e8c97c98bd1fb745cc4be1297f8626477645f
Tim Schäfer <ts+code@rcmd.org>
2022-05-10 14:59:03 +0200

    add calc_pure_python function

---
+++ highperf_python/julia_set/julia_set.py
@@ -0,0 +1,31 @@
+
+import time
+x1, x2, y1, y2 = -1.8, 1.8, -1.8, 1.8
+c_real, c_imag = -0.62772, -.42193
+
+
+def calc_pure_python(desired_width, max_iterations):
+    x_step = (x2 - x1) / desired_width
+    y_step = (y1 - y2) / desired_width
+    x = []
+    y = []
+    ycoord = y2
+    while ycoord > y1:
+        y.append(ycoord)
+        ycoord += y_step
+    xcoord = x1
+    while xcoord < x2:
+        x.append(xcoord)
+        xcoord += x_step
+    zs = []
+    cs = []
+    for ycoord in y:
+        for xcoord in x:
+            zs.append(complex(xcoord, ycoord))
+            cs.append(complex(c_real, c_imag))
+    start_time = time.time()
+    output = calculate_z_serial_purepython(max_iterations, zs, cs)
+    end_time = time.time()
+    secs = end_time - start_time
+    assert sum(output) == 33219980
+
