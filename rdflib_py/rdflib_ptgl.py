5a8dd9c973d05d8830a3a42346f9962166e69b3a
Tim Schaefer <ts+code@rcmd.org>
2023-10-05 10:31:06 +0200

    NEW: add rdflib python script

---
+++ rdflib_py/rdflib_ptgl.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+# rdflib_ptgl.py -- Test rdflib with an imaginary RDF vocabulary for the Protein Topology Graph Library (PTGL)
+# This is a toy example to try rdflib, ignore.
+
