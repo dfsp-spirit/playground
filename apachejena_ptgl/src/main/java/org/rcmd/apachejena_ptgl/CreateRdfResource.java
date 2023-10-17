bcf5493964af0c7d73ece42fd667b3a436488d1d
Tim Schaefer <ts+code@rcmd.org>
2023-10-17 14:04:15 +0200

    NEW: print model statements

--- apachejena_ptgl/src/main/java/org/rcmd/apachejena_ptgl/CreateRdfResource.java
+++ apachejena_ptgl/src/main/java/org/rcmd/apachejena_ptgl/CreateRdfResource.java
@@ -8,28 +8,65 @@ package org.rcmd.apachejena_ptgl;
  * This roughly follows the Apache Jena RDF API tutorial, see https://jena.apache.org/tutorials/rdf_api.html
  */
 
- import org.apache.jena.rdf.model.Model;
- import org.apache.jena.rdf.model.ModelFactory;
- import org.apache.jena.rdf.model.Resource;
- import org.apache.jena.vocabulary.VCARD;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.StmtIterator;
+import org.apache.jena.vocabulary.VCARD;
+import org.apache.jena.rdf.model.Statement;
 
  public class CreateRdfResource {
 
-    public static void createAuthor () {
-        // some definitions
-            final String personURI    = "http://somewhere/JohnSmith";
-            final String fullName     = "John Smith";
+    public static void main() {
 
-            // create an empty Model
-            Model model = ModelFactory.createDefaultModel();
+        Model model = ModelFactory.createDefaultModel();
+        Resource author = CreateRdfResource.createAuthor(model);
+        CreateRdfResource.printStatements(null);
+        
 
-            // create the resource
-            Resource johnSmith = model.createResource(personURI);
-
-            // add the property
-            johnSmith.addProperty(VCARD.FN, fullName);
     }
 
- }
+    public static Resource createAuthor (Model model) {
+        // some definitions
+        final String personURI    = "http://somewhere/JohnSmith";
+        final String fullName     = "John Smith";
+
+        // create the resource
+        Resource johnSmith = model.createResource(personURI);
+
+        // add the property
+        johnSmith.addProperty(VCARD.FN, fullName);
+
+        return johnSmith;
+    }
+
+    public static void printStatements(Model model) {
+        // list the statements in the Model
+        StmtIterator iter = model.listStatements();
+
+        // print out the predicate, subject and object of each statement
+        while (iter.hasNext()) {
+            Statement stmt      = iter.nextStatement();  // get next statement
+            Resource  subject   = stmt.getSubject();     // get the subject
+            Property  predicate = stmt.getPredicate();   // get the predicate
+            RDFNode   object    = stmt.getObject();      // get the object
+
+            System.out.print(subject.toString());
+            System.out.print(" " + predicate.toString() + " ");
+            if (object instanceof Resource) {
+            System.out.print(object.toString());
+            } else {
+                // object is a literal
+                System.out.print(" \"" + object.toString() + "\"");
+            }
+
+            System.out.println(" .");
+        }
+    }
+
+
+}
 
  
