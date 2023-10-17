7ba843934f4bd56220a290d0575f2de5129e7b93
Tim Schaefer <ts+code@rcmd.org>
2023-10-17 13:51:00 +0200

    CHG: add imports, create a resource

--- apachejena_ptgl/src/main/java/org/rcmd/apachejena_ptgl/CreateRdfResource.java
+++ apachejena_ptgl/src/main/java/org/rcmd/apachejena_ptgl/CreateRdfResource.java
@@ -1,6 +1,35 @@
 
 
+package org.rcmd.apachejena_ptgl;
+
+
 /**
- * Roughly follow the Apache Jena RDF API tutorial, see https://jena.apache.org/tutorials/rdf_api.html
  * 
+ * This roughly follows the Apache Jena RDF API tutorial, see https://jena.apache.org/tutorials/rdf_api.html
  */
+
+ import org.apache.jena.rdf.model.Model;
+ import org.apache.jena.rdf.model.ModelFactory;
+ import org.apache.jena.rdf.model.Resource;
+ import org.apache.jena.vocabulary.VCARD;
+
+ public class CreateRdfResource {
+
+    public static void createAuthor () {
+        // some definitions
+            final String personURI    = "http://somewhere/JohnSmith";
+            final String fullName     = "John Smith";
+
+            // create an empty Model
+            Model model = ModelFactory.createDefaultModel();
+
+            // create the resource
+            Resource johnSmith = model.createResource(personURI);
+
+            // add the property
+            johnSmith.addProperty(VCARD.FN, fullName);
+    }
+
+ }
+
+ 
