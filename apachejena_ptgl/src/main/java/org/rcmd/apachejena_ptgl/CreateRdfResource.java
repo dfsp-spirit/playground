aabdb1c3bc5ae857aae44525e9e7a049d65d07b3
Tim Schaefer <ts+code@rcmd.org>
2023-10-18 12:40:11 +0200

    CHG: add args to main func

--- apachejena_ptgl/src/main/java/org/rcmd/apachejena_ptgl/CreateRdfResource.java
+++ apachejena_ptgl/src/main/java/org/rcmd/apachejena_ptgl/CreateRdfResource.java
@@ -1,37 +1,32 @@
 
-
 package org.rcmd.apachejena_ptgl;
 
-
 /**
- * 
  * This roughly follows the Apache Jena RDF API tutorial, see https://jena.apache.org/tutorials/rdf_api.html
  */
-
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.rdf.model.StmtIterator;
 import org.apache.jena.vocabulary.VCARD;
-import org.apache.jena.rdf.model.Statement;
 
- public class CreateRdfResource {
+public class CreateRdfResource {
 
-    public static void main() {
+    public static void main(String[] args) {
 
         Model model = ModelFactory.createDefaultModel();
         Resource author = CreateRdfResource.createAuthor(model);
         CreateRdfResource.printStatements(null);
-        
 
     }
 
-    public static Resource createAuthor (Model model) {
+    public static Resource createAuthor(Model model) {
         // some definitions
-        final String personURI    = "http://somewhere/JohnSmith";
-        final String fullName     = "John Smith";
+        final String personURI = "http://somewhere/JohnSmith";
+        final String fullName = "John Smith";
 
         // create the resource
         Resource johnSmith = model.createResource(personURI);
@@ -48,16 +43,17 @@ import org.apache.jena.rdf.model.Statement;
 
         // print out the predicate, subject and object of each statement
         while (iter.hasNext()) {
-            Statement stmt      = iter.nextStatement();  // get next statement
-            Resource  subject   = stmt.getSubject();     // get the subject
-            Property  predicate = stmt.getPredicate();   // get the predicate
-            RDFNode   object    = stmt.getObject();      // get the object
+            Statement stmt = iter.nextStatement(); // get next statement
+            Resource subject = stmt.getSubject(); // get the subject
+            Property predicate = stmt.getPredicate(); // get the predicate
+            RDFNode object = stmt.getObject(); // get the object
 
             System.out.print(subject.toString());
             System.out.print(" " + predicate.toString() + " ");
             if (object instanceof Resource) {
-            System.out.print(object.toString());
-            } else {
+                System.out.print(object.toString());
+            }
+            else {
                 // object is a literal
                 System.out.print(" \"" + object.toString() + "\"");
             }
@@ -66,7 +62,4 @@ import org.apache.jena.rdf.model.Statement;
         }
     }
 
-
 }
-
- 
