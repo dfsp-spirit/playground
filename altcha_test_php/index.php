f4a82c6c53785b2995bd40aa6eee982e64d9ec5d
Tim Schaefer <ts+code@rcmd.org>
2024-06-14 16:00:09 +0200

    NEW: add the form with widget, works

--- altcha_test_php/index.php
+++ altcha_test_php/index.php
@@ -8,11 +8,24 @@ $app = AppFactory::create();
 
 // Include CORS middleware
 require __DIR__ . '/cors.php';
+require __DIR__ . '/altcha.php';
+
+$algo = "SHA-256";
+$secret_key = "bananeinderbirne";
+$my_altcha = new Altcha($algo, $secret_key, 1, 100);
+
 
 // Define your API routes
 $app->get('/hello/{name}', function ($request, $response, $args) {
     $name = $args['name'];
     $response->getBody()->write("Hello, $name");
+    return $response;
+});
+
+$app->get('/altcha_challenge', function ($request, $response, $args) {
+    global $my_altcha;
+    $challenge = $my_altcha->createChallenge();
+    $response->getBody()->write(json_encode($challenge));
     return $response;
 });
 
